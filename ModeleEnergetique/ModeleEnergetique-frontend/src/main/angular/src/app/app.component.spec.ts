52ac9457f4042930ed24d6194ac27cffa886cb13
yassinelaghouazi <yassinelaghouazi@gmail.com>
2020-04-24 16:39:40 +0000

    Material template is added

--- ModeleEnergetique/ModeleEnergetique-frontend/src/main/angular/src/app/app.component.spec.ts
+++ ModeleEnergetique/ModeleEnergetique-frontend/src/main/angular/src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular'`, () => {
+  it(`should have as title 'dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('dashboard');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('dashboard app is running!');
   });
 });
